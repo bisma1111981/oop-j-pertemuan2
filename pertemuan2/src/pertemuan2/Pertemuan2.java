77dc7bc5c08517452219349d0cf79938b530514a
bismadiana <semesterempat95@gmail.com>
2025-03-11 21:55:09 +0800

    pertemuan2

---
+++ pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,25 @@
+
+package pertemuan2;
+
+/**
+ *
+ * @author Bisma Diana
+ * tgl : 11 Maret 2015
+ */
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Dewa";
+        String nim = "2301010401";
+        
+       System.out.println("Pertemuan 2 Java");
+       System.out.println("~~~~~~~~~~~~~~~");
+       System.out.println("Nama   : "+nama);
+       System.out.println("NIM    : "+nim);
+       System.out.printf("Nama saya %s denga NIM %s, saat ini sedang belajar pemrograman bahasa java",nama,nim);
+    }
+    
+}
